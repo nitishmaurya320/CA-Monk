b90c088a787b709f9c9bfe77119a789ce9cae19f
Nitish Maurya <nitishmaurya2255@gmail.com>
2026-01-18 04:51:11 +0530

    added create blog page and its functions

--- src/pages/CreateBlog.tsx
+++ src/pages/CreateBlog.tsx
@@ -1,40 +1,112 @@
-import React from 'react'
-import {
-  Field,
-  FieldContent,
-  FieldDescription,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-  FieldLegend,
-  FieldSeparator,
-  FieldSet,
-  FieldTitle,
-} from "@/components/ui/field"
+
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea";
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+
+const createBlog=async(data:any)=>{
+    const res=await fetch("http://localhost:3001/blogs",{
+        method:"POST",
+        headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+    });
+      if (!res.ok) throw new Error("Failed to create blog");
+      return res.json();
+
+}
 const CreateBlog = () => {
+    type blog={
+        title:string,
+        category:string;
+        description:string,
+        coverImage:string,
+        content:string
+
+    }
+    const [form,setForm]=useState<blog>({
+        title:"",
+        category:"",
+        description:"",
+        coverImage:"",
+        content:""
+    })
+    const queryClient = useQueryClient();
+      const { mutate, isPending, isError } = useMutation({
+    mutationFn: createBlog,
+    onSuccess: () => {
+      // 🔥 THIS IS THE IMPORTANT PART
+      queryClient.invalidateQueries({ queryKey: ["blogs"] });
+      setForm({
+        title: "",
+        category:"",
+        description: "",
+        coverImage: "",
+        content: "",
+      });
+    },
+  });
+    const handleChange=(e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)=>{
+        const {name,value}=e.target
+
+        setForm((prev)=>({...prev,[name]:value}))
+    }
+     const handleSubmit = () => {
+        const data={
+            ...form,category:form.category.split(",").map((c)=>c.trim())
+        }
+    mutate(data);
+  };
+
   return (
-    <div className='px-10'>
-            <FieldSet>
-  <FieldLegend>Profile</FieldLegend>
-  <FieldDescription>This appears on invoices and emails.</FieldDescription>
-  <FieldGroup>
-    <Field>
-      <FieldLabel htmlFor="name">Full name</FieldLabel>
-      <Input id="name" autoComplete="off" placeholder="Evil Rabbit" />
-      <FieldDescription>This appears on invoices and emails.</FieldDescription>
-    </Field>
-    <Field>
-      <FieldLabel htmlFor="username">Username</FieldLabel>
-      <Input id="username" autoComplete="off" aria-invalid />
-      <FieldError>Choose another username.</FieldError>
-    </Field>
-    <Field orientation="horizontal">
-      
-      <FieldLabel htmlFor="newsletter">Subscribe to the newsletter</FieldLabel>
-    </Field>
-  </FieldGroup>
-</FieldSet>
+     <div className="max-w-2xl mx-auto space-y-6 p-6 bg-white rounded-lg">
+      <h1 className="text-2xl font-bold">Create New Blog</h1>
+
+      {/* Title */}
+      <Input
+        name="title"
+        placeholder="Blog title"
+        value={form.title}
+        onChange={handleChange}
+      />
+
+      {/* Category */}
+      <Input
+        name="category"
+        placeholder="Category (FINANCE, TECH)"
+        value={form.category}
+        onChange={handleChange}
+      />
+
+      {/* Description */}
+      <Textarea
+        name="description"
+        placeholder="Short description"
+        value={form.description}
+        onChange={handleChange}
+      />
+
+      {/* Cover Image */}
+      <Input
+        name="coverImage"
+        placeholder="Cover image URL"
+        value={form.coverImage}
+        onChange={handleChange}
+      />
+
+      {/* Content */}
+      <Textarea
+        name="content"
+        placeholder="Full blog content"
+        className="min-h-[150px]"
+        value={form.content}
+        onChange={handleChange}
+      />
+
+      <Button onClick={handleSubmit} className="w-full">
+        Create Blog
+      </Button>
     </div>
   )
 }
